cd50929683e257284fadf2c3bd265c2ef3463693
Charley <charleylla@foxmail.com>
2018-07-06 19:23:30 +0800

    增加几个 scss 文件

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -9,23 +9,30 @@ module.exports = function(env){
     mode:"development",
     devServer:{
       contentBase:resolve("dist"),
+      host:"0.0.0.0",
+      useLocalIp: true,
       overlay:{
         errors:true,
         warnings:true
       },
+      open:true,
       hot:true,
-    },
-    stats:{
-      assets: false,
-      chunks: false,
-      chunkGroups: false,
-      chunkModules: false,
-      chunkOrigins: false,
-      modules: false,
-      moduleTrace: false,
-      source: false,
-      builtAt: false,
-      children: false,
+      historyApiFallback: true,
+      inline: true,
+      disableHostCheck: true,
+      stats:{
+        assets: false,
+        chunks: false,
+        chunkGroups: false,
+        chunkModules: false,
+        chunkOrigins: false,
+        modules: false,
+        moduleTrace: false,
+        source: false,
+        builtAt: false,
+        children: false,
+        hash:false,
+      },
     },
     plugins:[
       //热更新
